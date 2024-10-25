e66ce26800830bdd3d4dacdf26e169639f16eadf
Maciej <maciejbaginski2008@icloud.com>
2024-10-25 20:38:52 +0200

    quick fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
 "use client"
-import Step2 from "@/components/auth/register/Step2";
 
 export default function Home() {
   return (
       <div className='w-dvw h-dvh flex items-center justify-center flex-col gap-4'>
 
-
-              <Step2/>
       </div>
   );
 }
